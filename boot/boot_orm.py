0021d08520b8caa4113c0c6373e3106f27d85e11
kishan gajjar <kishanegajjar@gmail.com>
2016-03-13 17:25:35 +0530

    [IMP] AUTO-compile less and re-compile less if any changes are made

--- boot/boot_orm.py
+++ boot/boot_orm.py
@@ -1,5 +1,12 @@
 from tools.config import config_options
 from orm import db
+import os
+import subprocess
+
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 # this variable hold value wether server run first time
 new_born = not db._table_exist("system_info")
@@ -7,3 +14,12 @@ new_born = not db._table_exist("system_info")
 if new_born:
     db._create_table("system_info")
     db._commit()
+
+cwd = os.getcwd()
+source = cwd + '/modules/static/less/build.less'
+dest = cwd + '/modules/static/css/bootstrap.css'
+try:
+    subprocess.Popen(['lessc', source, dest])
+    logger.info("New Less compiled")
+except:
+    logger.error("Error in less compile")
--- pyweb_server.py
+++ pyweb_server.py
@@ -2,10 +2,13 @@ from tools.config import config_options
 import boot
 import modules
 
+
+extra_files = ['modules/static/less/build.less', 'modules/static/less/custom.less']
 # flask server
 from modules.flask_app import web_path
 
 if config_options.log_level == "DEBUG":
     web_path.debug = True
 
-web_path.run()
+
+web_path.run(extra_files=extra_files)
